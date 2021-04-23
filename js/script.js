877fee62959486bbeaf1b466ec07e86b97e89e47
Faijan-Momin <faijanmomin99@gmail.com>
2021-04-23 12:31:01 +0530

    first commit

--- js/script.js
+++ js/script.js
@@ -3,6 +3,6 @@ function ModalShow(){
     window.location.href = "#modal";
 }
 function HideModal(){
-    document.getElementById("modal").style.zIndex = "3";
+    document.getElementById("modal").style.zIndex = "-1";
     window.location.href = "#";
 }
