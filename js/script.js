761109caf15ec8e5d2d4bee089eef2832f76ea8f
Faijan-Momin <faijanmomin99@gmail.com>
2021-04-24 13:35:35 +0530

    first commit

--- js/script.js
+++ js/script.js
@@ -6,3 +6,13 @@ function HideModal(){
     document.getElementById("modal").style.zIndex = "-1";
     window.location.href = "#";
 }
+
+function showDrpDwn(){
+    var drpdwn = document.getElementById("subject-dashboard-profile-drowdown");
+    if(drpdwn.style.display == "block"){
+        drpdwn.style.display = "none";
+    }
+    else{
+        drpdwn.style.display = "block";
+    }
+}
